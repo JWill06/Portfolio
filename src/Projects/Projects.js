7af4e0be4356e2e08a41c0aa5f2b1cf035c20bdb
jordan williamson <williamsonjordan05@gmail.com>
2024-06-26 15:50:08 -0600

    added deployed links for two projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -3,6 +3,7 @@ import js from '../images/icons8-js.svg'
 import css from '../images/icons8-css-100.svg'
 import html from '../images/icons8-html.svg'
 import './Projects.css'
+import { NavLink } from 'react-router-dom'
 
 const Projects = ({project}) => {
     return (
@@ -19,10 +20,13 @@ const Projects = ({project}) => {
                 <img className='icons' alt='html icon' src={html} />
             <div>
                 </div>
-                <p>GitHub repo link, click on the icon below!</p>
+                <p>GitHub repo link, click on the GitHub icon below!</p>
                 <a href={project.repo_link}>
                     <img className='link' alt='github link'src={GitHub} />
                 </a>
+                {project.deployed_link === "https://jwill06.github.io/tic-tac-toe/" ? <div><p>Click on the Taco emoji to play!</p> <a className="deployedLink" href={project.deployed_link}>🌮</a></div> : null}
+                {project.deployed_link === "https://jwill06.github.io/coloRandom/" ? <div><p>Click on any Colored Box emoji to make some palletts!</p> <a className="deployedLink" href={project.deployed_link}>🟦 🟧 🟨 🟩</a></div> : null}
+
             </div>
             {project.funny ? <p className='hidden funny'>{project.funny}</p> : null}
         </div>
