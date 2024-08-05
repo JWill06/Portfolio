947f1add3f60ef4cb410dfbcb7cfafc98b7b5494
jordan williamson <williamsonjordan05@gmail.com>
2024-08-05 17:57:24 -0600

    updated projects and styling

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -2,30 +2,42 @@ import GitHub from '../images/icons8-github.svg'
 import js from '../images/icons8-js.svg'
 import css from '../images/icons8-css-100.svg'
 import html from '../images/icons8-html.svg'
+import react from '../images/icons8-react-js-40.png'
+import typescript from '../images/icons8-typescript-48.png'
 import './Projects.css'
 
 const Projects = ({project}) => {
+    const frameWorks = [
+        { name: "HTML", icon: html },
+        { name: "CSS", icon: css },
+        { name: "JavaScript", icon: js },
+        { name: "React", icon: react },
+        { name: "Typescript", icon: typescript}
+    ]
     return (
         <div className='preview'>
             <div className='imageContainer'>
                 <img className="gif" src={`${process.env.PUBLIC_URL}${project.gif}`} alt="preview" />
             </div>
             <div className='text'>
-                <h1>{project.name}</h1>
+            <h1>{project.name}</h1>
                 <p>{project.description}</p>
                 <h2>Built with</h2>
-                <img className='icons' alt='javascript icon' src={js} />
-                <img className='icons' alt='css icon' src={css} />
-                <img className='icons' alt='html icon' src={html} />
+                {frameWorks.map(({ name, icon }) => 
+                    project.built.includes(name) ? (
+                        <img className='icons' alt={name} src={icon} />
+                    ) : null
+                )}
             <div>
                 </div>
-                <p>GitHub repo link, click on the GitHub icon below!</p>
+                <p><strong>GitHub repo link, click on the GitHub icon below!</strong></p>
                 <a href={project.repo_link}>
                     <img className='link' alt='github link'src={GitHub} />
                 </a>
-                {project.deployed_link === "https://jwill06.github.io/tic-tac-toe/" ? <div><p>Click on the Taco emoji to play!</p> <a className="deployedLink" href={project.deployed_link}>🌮</a></div> : null}
-                {project.deployed_link === "https://jwill06.github.io/coloRandom/" ? <div><p>Click on any Colored Box emoji to make some pallettes!</p> <a className="deployedLink" href={project.deployed_link}>🟦 🟧 🟨 🟩</a></div> : null}
-
+                {project.deployed_link === "https://jwill06.github.io/tic-tac-toe/" ? <div><p><strong>Click on the Taco emoji to play!</strong></p> <a className="deployedLink" href={project.deployed_link}>🌮</a></div> : null}
+                {project.deployed_link === "https://jwill06.github.io/coloRandom/" ? <div><p><strong>Click on any Colored Box emoji to make some pallettes!</strong></p> <a className="deployedLink" href={project.deployed_link}>🟦 🟧 🟨 🟩</a></div> : null}
+                {project.deployed_link === "https://art-collector-ten.vercel.app" ? <div><p><strong>Click on the art to see some art!</strong></p> <a className="deployedLink" href={project.deployed_link}>🖼️</a></div> : null}
+                {project.deployed_link === "https://art-collector-ten.vercel.app/" ? <div><p><strong>Click on the remote workers to find some postings!</strong></p> <a className="deployedLink" href={project.deployed_link}>👨🏽‍💻👨🏻‍💻👩🏼‍💻👩🏽‍💻</a></div> : null}
             </div>
             {project.funny ? <p className='hidden funny'>{project.funny}</p> : null}
         </div>
