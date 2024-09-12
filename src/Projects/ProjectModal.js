ef3289ff1d13a57899852d3824eac8b19c0921af
jordan williamson <williamsonjordan05@gmail.com>
2024-09-11 19:57:06 -0600

    added other projects and updated case management for specific rendering

--- src/Projects/ProjectModal.js
+++ src/Projects/ProjectModal.js
@@ -21,6 +21,43 @@ export const ProjectModal = ({project, setIsOpen}) => {
     const handleClick = () => {
         navigate(project.repo_link)
     }
+    const getDeployedLinkText = (link) => {
+        switch(link) {
+            case "https://jwill06.github.io/tic-tac-toe/":
+                return "Click on the Taco emoji to play!";
+            case "https://jwill06.github.io/coloRandom/":
+                return "Click on any Colored Box emoji to make some pallettes!";
+            case "https://art-collector-ten.vercel.app":
+                return "Click on the art to see some art!";
+            case "https://remote-job-locator.vercel.app/":
+                return "Click on the remote workers to find some postings!";
+            case "https://www.portersflooring-remodeling.com/":
+                return "Click on the hammers to see some flooring!";
+            case "https://www.handmadebyhaylee.com/":
+                return "Click on the art to see some art!";
+            default:
+                return "Click to view the deployed project!";
+        }
+    }
+    
+    const getDeployedLinkEmoji = (link) => {
+        switch(link) {
+            case "https://jwill06.github.io/tic-tac-toe/":
+                return "🌮";
+            case "https://jwill06.github.io/coloRandom/":
+                return "🟦 🟧 🟨 🟩";
+            case "https://art-collector-ten.vercel.app":
+                return "🖼️";
+            case "https://remote-job-locator.vercel.app/":
+                return "👨🏽‍💻👨🏻‍💻👩🏼‍💻👩🏽‍💻";
+            case "https://www.portersflooring-remodeling.com/":
+                return "🔨🔨";
+            case "https://www.handmadebyhaylee.com/":
+                return "🧶🧶";
+            default:
+                return "🔗";
+        }
+    }
 
   return (
     <div className='modal' onClick={() => !setIsOpen}>
@@ -38,18 +75,23 @@ export const ProjectModal = ({project, setIsOpen}) => {
                 <div>
                     </div>
                     <div className='linksContainer'>
+                        {project.repo_link ? 
                         <div className='repoLinkContainer'>
                         <p><strong>GitHub repo link, click on the GitHub icon below!</strong></p>
                         <a href={project.repo_link} onClick={handleClick}>
                         <img className='link' alt='github link'src={GitHub} />
                     </a>
                     </div>
+                     : <p>This repo is currently private</p>}
                     <div className='deployedLinksContainer'>
-
-                    {project.deployed_link === "https://jwill06.github.io/tic-tac-toe/" ? <div><p><strong>Click on the Taco emoji to play!</strong></p> <a className="deployedLink" href={project.deployed_link}>🌮</a></div> : null}
-                    {project.deployed_link === "https://jwill06.github.io/coloRandom/" ? <div><p><strong>Click on any Colored Box emoji to make some pallettes!</strong></p> <a className="deployedLink" href={project.deployed_link}>🟦 🟧 🟨 🟩</a></div> : null}
-                    {project.deployed_link === "https://art-collector-ten.vercel.app" ? <div><p><strong>Click on the art to see some art!</strong></p> <a className="deployedLink" href={project.deployed_link}>🖼️</a></div> : null}
-                    {project.deployed_link === "https://remote-job-locator.vercel.app/" ? <div><p><strong>Click on the remote workers to find some postings!</strong></p> <a className="deployedLink" href={project.deployed_link}>👨🏽‍💻👨🏻‍💻👩🏼‍💻👩🏽‍💻</a></div> : null}
+                    {project.deployed_link && (
+                        <div>
+                            <p><strong>{getDeployedLinkText(project.deployed_link)}</strong></p>
+                            <a className="deployedLink" href={project.deployed_link} target="_blank" rel="noopener noreferrer">
+                                {getDeployedLinkEmoji(project.deployed_link)}
+                            </a>
+                        </div>
+                    )}
                     </div>
                     </div>
                 </div>
